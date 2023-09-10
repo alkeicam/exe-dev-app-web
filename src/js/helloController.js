d2e166140d9011b09a58083fa57d61ad7d5df5d2
Maciej Grula <maciej.grula@execon.pl>
2023-09-10 08:56:46 +0200

    login - wip

--- src/js/helloController.js
+++ src/js/helloController.js
@@ -1,10 +1,9 @@
 class HelloController {
     
-    constructor(emitter, container) {
+    constructor(emitter) {
         this.CONST = {
         }
-        this.emitter = emitter
-        this.container = container;
+        this.emitter = emitter        
         
         
         
@@ -26,7 +25,14 @@ class HelloController {
                         code: 0,
                         message: "OK"
                     }
-                }                
+                },
+                f2: {
+                    v: "",
+                    e: {
+                        code: 0,
+                        message: "OK"
+                    }
+                }
             }
         }
     }
@@ -37,15 +43,15 @@ class HelloController {
         return text.substring(0,18)+"..."+text.substr(text.length-28, text.length)
     }
 
-    static async getInstance(emitter, container){
-        const a = new HelloController(emitter, container)
-        // electronAPI.listenerAPI.onEventsSync(async (_event, message)=>{
-        // const effortData = await electronAPI.API.effort();                        
+    static async getInstance(emitter){
+        const a = new HelloController(emitter)
+        
         return a;
     }
 
     async handleJoin(e, that){
         try{
+          
             that.model.notBusy = false;
             const account = await electronAPI.API.authJoin(that.model.forms.f1.v);  
             that.model.notBusy = true;
@@ -58,15 +64,37 @@ class HelloController {
         
     }
 
-    async handleInput(e, that){
-        if(that.model.forms.f1.v && that.model.forms.f1.v.length>=6){
-            that.model.forms.f1.e.code=0
-            that.model.forms.f1.e.message="OK"
-        }else{
-            that.model.forms.f1.e.code=1
-            that.model.forms.f1.e.message="Invalid invitation code"
+    async _validateField(field){
+        switch(field){
+            case "f1":
+                if(this.model.forms[field].v.match(/(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi)){
+                    this.model.forms[field].e.code=0
+                    this.model.forms[field].e.message="OK"
+                }else{
+                    this.model.forms[field].e.code=1
+                    this.model.forms[field].e.message="Please provide valid email address"
+                }
+
+                break;
+            case "f2":
+                if(this.model.forms[field].v && this.model.forms[field].v.length>=1){
+                    this.model.forms[field].e.code=0
+                    this.model.forms[field].e.message="OK"
+                }else{
+                    this.model.forms[field].e.code=1
+                    this.model.forms[field].e.message="Please provide password"
+                }
+                break;
         }
-            
+    }
+
+    async handleInput(e, that){
+        const field = e.target.dataset.fieldId;
+        
+        await that._validateField(field)
+        
+        
+ 
     }
 
     getQueryParam(paramName){
